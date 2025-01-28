e38da82c0db1159ff7bcf66f8d23f8bbdc5dff4c
Michael Waterworth <glastohacks@gmail.com>
2025-01-28 22:07:21 +0000

    Add basic plotter generator code for Mandelbrot fractals
    This has been rewritten from Python following this tutorial as inspiration
    https://realpython.com/mandelbrot-set-python/

--- main.go
+++ main.go
@@ -1,17 +1,107 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"flag"
+	"fmt"
+	"image/color"
+	"math/cmplx"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
-    })
+	// Define a string flag with a default value and a short description.
+	input := flag.String("input", "", "Input value to be processed")
 
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// Parse the command-line flags.
+	flag.Parse()
 
-    http.ListenAndServe(":80", nil)
+	// Check if the input flag was provided.
+	if *input == "" {
+		fmt.Println("Please provide an input value using the -input flag.")
+		return
+	}
+
+	processInput()
+}
+
+func processInput() (int64, error) {
+	c := complexMatrix(-2, 0.5, -1.5, 1.5, 1024)
+	members := getMembers(c, 20)
+
+	scatterData := generatePoints(members)
+
+	p := plot.New()
+
+	p.Title.Text = "Mandelbrot Set"
+
+	// Make a scatter plotter and set its style.
+	s, err := plotter.NewScatter(scatterData)
+	if err != nil {
+		panic(err)
+	}
+	s.GlyphStyle.Color = color.RGBA{R: 255, B: 128, A: 255}
+
+	s.GlyphStyle.Radius = vg.Points(0.1)
+
+	p.Add(s)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(1*1000, 1*1000, "points.png"); err != nil {
+		panic(err)
+	}
+	return 0, nil
+}
+
+func complexMatrix(xmin, xmax, ymin, ymax float64, pixelDensity int) [][]complex128 {
+	re := linspace(xmin, xmax, int((xmax-xmin)*float64(pixelDensity)))
+	im := linspace(ymin, ymax, int((ymax-ymin)*float64(pixelDensity)))
+	matrix := make([][]complex128, len(im))
+	for i := range matrix {
+		matrix[i] = make([]complex128, len(re))
+		for j := range matrix[i] {
+			matrix[i][j] = complex(re[j], im[i])
+		}
+	}
+	return matrix
+}
+
+func linspace(start, end float64, num int) []float64 {
+	step := (end - start) / float64(num-1)
+	result := make([]float64, num)
+	for i := 0; i < num; i++ {
+		result[i] = start + float64(i)*step
+	}
+	return result
+}
+
+func isStable(c complex128, numIterations int) bool {
+	z := complex(0, 0)
+	for i := 0; i < numIterations; i++ {
+		z = z*z + c
+	}
+	return cmplx.Abs(z) <= 2
+}
+
+func getMembers(c [][]complex128, numIterations int) []complex128 {
+	var members []complex128
+	for _, row := range c {
+		for _, val := range row {
+			if isStable(val, numIterations) {
+				members = append(members, val)
+			}
+		}
+	}
+	return members
+}
+
+func generatePoints(ns []complex128) plotter.XYs {
+	pts := make(plotter.XYs, len(ns))
+	for i := range pts {
+		pts[i].X = real(ns[i])
+		pts[i].Y = imag(ns[i])
+	}
+	return pts
 }
