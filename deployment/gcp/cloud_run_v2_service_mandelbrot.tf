9a7f1a2f8f143f4454ecc76d11f64d2dc3cbf865
Michael Waterworth <glastohacks@gmail.com>
2025-02-01 14:05:43 +0000

    Add GCP terraform deployment
    Fixes #14

---
+++ deployment/gcp/cloud_run_v2_service_mandelbrot.tf
@@ -0,0 +1,43 @@
+resource "google_cloud_run_v2_service" "mandelbrot" {
+  name     = "mandelbrot"
+  location = "europe-west2" // London
+  deletion_protection = true
+  ingress = "INGRESS_TRAFFIC_ALL"
+  project = "mandelbrot-tinker"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "8096Mi"
+        }
+      }
+      startup_probe {
+        initial_delay_seconds = 0
+        timeout_seconds = 1
+        period_seconds = 3
+        failure_threshold = 1
+        tcp_socket {
+          port = 8080
+        }
+      }
+      liveness_probe {
+        http_get {
+          path = "/livez"
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_binding" "mandelbrot" {
+  project = "mandelbrot-tinker"
+  location = google_cloud_run_v2_service.mandelbrot.location
+  service  = google_cloud_run_v2_service.mandelbrot.name
+  role     = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
+}
