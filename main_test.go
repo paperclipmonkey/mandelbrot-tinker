4e56f62965774a5b9b17336c1ac0a036e118a395
Michael Waterworth <glastohacks@gmail.com>
2025-02-07 11:57:50 +0000

    Improve testing based on hitting the mandelbrot endpoints with both good and bad data
    Endpoints tested:
    `livez`
    `health`
    `mandelbrot/z,x,y`

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -21,5 +24,108 @@ func TestSlippyToMandelbrot(t *testing.T) {
 			t.Errorf("slippyToMandelbrot(%d, %d, %d) = %f, %f, %f, %f; want %f, %f, %f, %f",
 				tt.z, tt.x, tt.y, xmin, ymin, xmax, ymax, tt.xmin, tt.ymin, tt.xmax, tt.ymax)
 		}
+	}
+}
+
+func TestLivezHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/livez", nil)
+	w := httptest.NewRecorder()
+	handleLivez(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "ok" {
+		t.Errorf("expected ok got %v", string(data))
+	}
+}
+
+func TestHealthzHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/healthz", nil)
+	w := httptest.NewRecorder()
+	handleHealthz(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "ok" {
+		t.Errorf("expected ok got %v", string(data))
+	}
+}
+
+func TestMandelbrotHandler(t *testing.T) {
+	tests := []struct {
+		name string
+		z    string
+		x    string
+		y    string
+	}{
+		{"origin", "0", "0", "0"},
+		{"zoom1_topleft", "1", "0", "0"},
+		{"zoom1_bottomright", "1", "1", "1"},
+		{"zoom2_center", "2", "1", "1"},
+		{"zoom3_detail", "3", "3", "3"},
+		{"zoom4_edge", "4", "8", "8"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/mandelbrot/", nil)
+			req.SetPathValue("z", tt.z)
+			req.SetPathValue("x", tt.x)
+			req.SetPathValue("y", tt.y)
+
+			w := httptest.NewRecorder()
+			handleMandelbrot(w, req)
+			res := w.Result()
+			defer res.Body.Close()
+
+			if res.StatusCode != http.StatusOK {
+				t.Errorf("expected status OK got %v", res.StatusCode)
+			}
+
+			contentType := res.Header.Get("Content-Type")
+			if contentType != "image/png" {
+				t.Errorf("expected Content-Type image/png got %v", contentType)
+			}
+		})
+	}
+}
+
+func TestMandelbrotValidationHandler(t *testing.T) {
+	tests := []struct {
+		name string
+		z    string
+		x    string
+		y    string
+	}{
+		{"text_in_int", "test", "10", "0"},
+		{"negative_zoom", "-1", "0", "0"},
+		{"deep_zoom", "100", "1", "1"},
+		{"out_of_bounds_x", "1", "4", "1"},
+		{"out_of_bounds_y", "1", "1", "4"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/mandelbrot/", nil)
+			req.SetPathValue("z", tt.z)
+			req.SetPathValue("x", tt.x)
+			req.SetPathValue("y", tt.y)
+
+			w := httptest.NewRecorder()
+			handleMandelbrot(w, req)
+			res := w.Result()
+			defer res.Body.Close()
+
+			if res.StatusCode != 400 {
+				t.Errorf("in test %v", tt.name)
+				t.Errorf("expected status 400 got %v", res.StatusCode)
+			}
+		})
 	}
 }
